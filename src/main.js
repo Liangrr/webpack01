778c9d8d240c4af258893e3c96e3bbc40eaca80e
asuria <6515887+asuria@user.noreply.gitee.com>
2024-07-08 11:55:46 +0800

    webpack重0到1

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+  return <div>Hello World112221</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+// 输入entry， 配置处理, 后处理，输出output
+
+module.exports = {
+  // 设置入口文件
+  entry: './src/main.js',
+  // 指定打包后的输出位置和文件名
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  // 设置模式：开发模式
+  mode: 'development',
+  // 设置开发服务器
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({ template: './public/index.html' })
+  ],
+  module: {
+    rules: [
+      {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+    },
+      {
+        test: /\.css$/,
+        // exclude: /node_modules/,
+        // 本地建议使用style-loader
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+      },
+    ]
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  }
+};
